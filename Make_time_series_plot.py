b36e0695222aa6742825c43091e8568c10b7e9ed
Elise Comeau <comeau.elise@courrier.uqam.ca>
2021-08-04 16:26:17 -0400

    Added four new parameters. The first determines what length of time is shown on the plot. The second specifies the name of the variable to be plotted. The third gives the index of the variable to be plotted in the data array. The fourth provides the id of the station from which the data was taken.

--- Make_time_series_plot.py
+++ Make_time_series_plot.py
@@ -1,39 +1,36 @@
 import fluxnet_classes as fc
 import numpy as np
 import matplotlib.pyplot as plt
+import pdb
 
 
-
-""" CONSTANTS """
-
-ROW_NBR_START = 0    # first entry that is plotted   
-ROW_NBR_STOP  = 20   # last entry that is plotted
-COLUMN_NBR   = 0     # column 0 corresponds to variable G; choice is arbitrary
-
-STATION_ID = '001_CA-DBB'     # station number, country, station name
-VAR_NAME   = 'G'
-VAR_UNIT   = '(W * m^(-2))'
-
-
-
-def Compare_min_data_percentage(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentages) :
+def Compare_min_data_percentage(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentages, time_length, var_name, var_index, station_id) :
 
     """
 
-    Creates a plot showing the values of different sets of data according to their respective dates, temporal frequency and minimum data percentage used to generate the data when applicable.
+    Creates a plot showing the values of different sets of data according to their respective dates, temporal frequency and minimum data percentage for a single station.
 
 
     Parameters :
 
-        data (array)                 : Data used for the plot. Since the data will be averaged over a longer period, enter the data with the shortest averaging period that will be presented in the plot.
+        data_short_period (array)    : Data used for the plot. Since the data will be averaged over a longer period, enter the data with the shortest averaging period that will be presented in the plot.
 
-        dates (array)                : Dates associated with data.
+        dates_short_period (array)   : Dates associated with data.
 
-        t_freq_short (int)           : Temporal frequency of data.
+        t_freq_short (int)           : Temporal frequency of data in hours.
 
-        t_freq_long (int)            : Temporal frequency of the data averaged over the longer period. 
+        t_freq_long (int)            : Temporal frequency of the data averaged over the longer period in hours. 
 
         min_data_percentages (array) : Minimum data percentage(s) that will be presented in the plot.
+
+        time_length (float)          : Amount of time to be presented in the plot in hours.
+
+        var_name (string)            : Name of the variable to be plotted (eg. 'G').
+
+        var_index (int)              : Index of the variable to be plotted (ie, the index of the column corresponding to the variable to be plotted). 
+
+        station_id (string)          : Identification of the station whose data will be plotted. This can include the station number, station name, etc. 
+
 
 
     Author        : Élise Comeau
@@ -45,42 +42,91 @@ def Compare_min_data_percentage(data_short_period, dates_short_period, t_freq_sh
     """
 
 
+    # Step 1 : Obtain dates to plot
 
-    # Step 1 : Plot the data and dates for the short period
-    
-    legends_list = []
+    initial_date = dates_short_period[0]
+    final_date   = initial_date + time_length * 3600   # 3600 to convert hours to seconds
+    time_step    = t_freq_short * 3600
 
-    # VÉRIFIE LIGNE EN-DESSOUS
-    plt.plot(dates_short_period[ROW_NBR_START:ROW_NBR_STOP], data_short_period[:20, COLUMN_NBR], linestyle=None, marker='s')
+    ref_times = np.arange(initial_date, final_date, time_step)
+    last_row  = ref_times.shape[0]                             # last row that will be plotted in data
 
-    legend_label_short_period =	t_freq_short + ' hrs avg'
-    legends_list.append(legend_label_short_period)
+#    pdb.set_trace()
 
-    print('Test')
 
-    # Step 2 : Plot the data and dates for the long period(s)
+    # Step 2 : Generate data and dates for the long period(s)
+
+    data_list  = [ data_short_period ]
+    dates_list = [ dates_short_period ]
+
+    legend      = str(t_freq_short) + ' hr avg' 
+    legend_list = [ legend ] 
 
     for min_data_percentage in min_data_percentages :
 
-        data_long_period, dates_long_period = fc.Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentage)
+        data_long_period, dates_long_period = fc.Temporal_mean(data_short_period, dates_short_period, t_freq_short * 3600, t_freq_long * 3600, min_data_percentage)
 
-        plt.plot(dates_long_period[:20], data_short_period[ROW_NBR_START:ROW_NBR_STOP, COLUMN_NBR], linestyle=None, marker='+')
+        data_list.append(data_long_period)
+        dates_list.append(dates_long_period)
 
-        legend_label_long_period = str(t_freq_long / (60 * 60)) + ' hrs avg (min. data percentage = ' + str(min_data_percentage) + '%)'
-        legends_list.append(legend_label_long_period)
+        legend = str(t_freq_long) + ' hr avg (min. data percentage = ' + str(min_data_percentage) + '%)'
+        legend_list.append(legend)
+
+#        pdb.set_trace()
+
+#    pdb.set_trace()
 
 
-    # Step 3 : Add labels to plot
+    # Step 3 : Add missing values
 
-    title = 'Value of ' + VAR_NAME + ' according to time, averaging period and minimum data percentage (station = ' + STATION_ID + ')'
+#    index = 0
+
+    for data, dates in zip(data_list, dates_list) :
+
+        for time in ref_times :
+
+            if ( time not in dates ) :
+
+#                print( str(time) + ' not found.')
+
+                insert_index = np.amin(np.where(dates > time))
+                dates        = np.insert(dates, insert_index, time)
+                data         = np.insert(data, insert_index, fc.constants.missing_value, axis=0)
+
+#                if ( index != 0 ) :
+#                    pdb.set_trace()
+
+#            else :
+
+#                print ( str(time) + ' found.') 
+
+        data = np.ma.masked_where(data == fc.constants.missing_value, data)
+
+
+        # Step 4 : Add data, dates to plot
+
+        plt.plot(dates[:last_row], data[:last_row, var_index], linestyle=None, marker='+')
+#            pdb.set_trace()
+
+#        index = index + 1
+
+#        pdb.set_trace()	
+
+
+    # Step 5 : Add labels to plot
+
+    var_unit = fc.variables.units[var_name]
+
+    title = 'Value of ' + var_name + ' according to time, averaging period and minimum data percentage (station = ' + station_id + ')'
     x_label = 'Time (s)'
-    y_label = 'Value of ' + VAR_NAME + ' ' + VAR_UNIT
+    y_label = 'Value of ' + var_name + ' (' + var_unit + ')' 
 
     plt.title(title, fontsize=14, fontweight='bold')
     plt.xlabel(x_label, fontsize=12, fontweight='bold')
     plt.ylabel(y_label, fontsize=12, fontweight='bold') 
-    plt.legend(legends_list)
+    plt.legend(legend_list)
 
+#    pdb.set_trace()
     plt.show()
  
 
