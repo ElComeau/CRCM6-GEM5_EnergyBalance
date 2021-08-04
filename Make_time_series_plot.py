4b4e0c937e752976cbed4b692b9194714ddbce10
Elise Comeau <comeau.elise@courrier.uqam.ca>
2021-08-04 16:21:38 -0400

    Added two new parameters that define the temporal freqeuncy of the data before and after it is processed.

--- Make_time_series_plot.py
+++ Make_time_series_plot.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 
 """ CONSTANTS """
 
-T_FREQ_SHORT = 30 * 60        # 30 mins * 60 mins/s = 180 s
-T_FREQ_LONG  = 3 * 60 * 60    # 3 hrs * 60 mins/hr * 60 s/min = 10800 s 
-
 ROW_NBR_START = 0    # first entry that is plotted   
 ROW_NBR_STOP  = 20   # last entry that is plotted
 COLUMN_NBR   = 0     # column 0 corresponds to variable G; choice is arbitrary
@@ -19,7 +16,7 @@ VAR_UNIT   = '(W * m^(-2))'
 
 
 
-def Compare_min_data_percentage(data_short_period, dates_short_period, min_data_percentages) :
+def Compare_min_data_percentage(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentages) :
 
     """
 
@@ -32,14 +29,18 @@ def Compare_min_data_percentage(data_short_period, dates_short_period, min_data_
 
         dates (array)                : Dates associated with data.
 
-        min_data_percentages (array) : The minimum data percentage(s) that will be presented in the plot.
+        t_freq_short (int)           : Temporal frequency of data.
+
+        t_freq_long (int)            : Temporal frequency of the data averaged over the longer period. 
+
+        min_data_percentages (array) : Minimum data percentage(s) that will be presented in the plot.
 
 
     Author        : Élise Comeau
 
     Created       : June 15th 2021
 
-    Last modified : June 16th 2021 
+    Last modified : June 17th 2021 
 
     """
 
@@ -49,21 +50,23 @@ def Compare_min_data_percentage(data_short_period, dates_short_period, min_data_
     
     legends_list = []
 
-    plt.plot(dates_short_period[ROW_NBR_START:ROW_NBR_STOP], data_short_period[:20, COLUMN_NBR], linestyle=None, marker='+')
+    # VÉRIFIE LIGNE EN-DESSOUS
+    plt.plot(dates_short_period[ROW_NBR_START:ROW_NBR_STOP], data_short_period[:20, COLUMN_NBR], linestyle=None, marker='s')
 
-    legend_label_short_period =	str(T_FREQ_SHORT / 60) + ' mins avg'
+    legend_label_short_period =	t_freq_short + ' hrs avg'
     legends_list.append(legend_label_short_period)
 
+    print('Test')
 
     # Step 2 : Plot the data and dates for the long period(s)
 
     for min_data_percentage in min_data_percentages :
 
-        data_long_period, dates_long_period = fc.Temporal_mean(data_short_period, dates_short_period, T_FREQ_SHORT, T_FREQ_LONG, min_data_percentage)
+        data_long_period, dates_long_period = fc.Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentage)
 
         plt.plot(dates_long_period[:20], data_short_period[ROW_NBR_START:ROW_NBR_STOP, COLUMN_NBR], linestyle=None, marker='+')
 
-        legend_label_long_period = str(T_FREQ_LONG / (60 * 60)) + ' hrs avg (min. data percentage = ' + str(min_data_percentage) + '%)'
+        legend_label_long_period = str(t_freq_long / (60 * 60)) + ' hrs avg (min. data percentage = ' + str(min_data_percentage) + '%)'
         legends_list.append(legend_label_long_period)
 
 
