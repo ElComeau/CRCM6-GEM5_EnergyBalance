500b484d420774e48cad1b2450dc57a7527354f3
Elise Comeau <comeau.elise@courrier.uqam.ca>
2021-08-04 18:02:02 -0400

    Added a very brief description of what the script does. Classified the constants according to type and then by alphabetical order. Removed call to plotting function, which should be done by another script specifically designed for this purpose.

--- Filter_amf_data_abridged.py
+++ Filter_amf_data_abridged.py
@@ -7,232 +7,192 @@ import Make_comparison_plots_v4 as mcp
 import pdb
 
 
+'''
+This scripts processes AmeriFlux (AMF) data at a 0.5 hour frequency. It removes unnecessary variables and data entries with missing values.
+'''
+
 
 # Step 0 : Define constants
 
-FILTERED_DATA_COMMON_DIRECTORY               = '/snow/diluca/FLUXNET_America/1990-2021/v1/TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE'
-FILTERED_DATA_SAMPLING_PERCENTAGES_DIRECTORY = 'sampling-percentage'
-FILTERED_DATA_MIN_NBR_YRS_DIRECTORY          = 'num-years'
+# Step 0.1 : Define directories
 
-SAMPLING_PERCENTAGES = ['25', '50', '75']  # minimum sampling percentages
-MIN_NBR_YRS          = ['1', '2', '5']     # minimum number of years of available data
-
-FILTERED_DATA_FILENAME  = 'AMF_summary_1990-2021_TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE.txt'
-STATION_NAMES_FILENAME  = 'AMF_station-filelist_1990-2021_TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE.txt'
-
-DELIMITER_STATION_NAMES = '_'
-
-NBR_OF_HEADER_ROWS_FILTERED_DATA = 3
-DELIMITER_FILTERED_DATA          = ' '
-NEW_LINE_CHAR                    = '\n'
-NULL_CHAR                        = ''
-
-UNFILTERED_DATA_DIRECTORY_R        = '/home/data/Validation/AmeriFlux'
-NBR_OF_HEADER_ROWS_UNFILTERED_DATA = 3
-UNFILTERED_DATA_DIRECTORY_W        = '/snow/comeau/FLUXNET_America/AMF_1990-2019'
-
-UNFILTERED_DATA_FILENAME_1 = 'AMF_data_'
-UNFILTERED_DATA_FILENAME_2 = '_1990-2021_'
-UNFILTERED_DATA_FILENAME_3 = '_0-5.npy'
-
-Y_AXIS_LABEL            = 'Nbr of years available'
-LABEL_PRE_PROCESSING    = 'AMF -  30 mins'
-LABEL_POST_PROCESSING_1 = 'AMF - 180 mins ('
-LABEL_POST_PROCESSING_2 = ' yr(s), '
-LABEL_POST_PROCESSING_3 = '%)' 
-CAPTION                 = '''Number of years of available AmeriFlux (AMF) data according to AMF station. Stations are referred to by their id number, country (Canada or United States) and station name.
-a) Number of years of available data. Blue bars represent data averaged over 30 minutes, corresponding to pre-processed data. Orange bars represent data averaged over
-180 minutes, ie the post-processing results. Processing involved two criteria : 1) A minimum number of years of data must be available at the station; 2) 180 minutes average is
-calculated only if more than a certain percentage of the corresponding 30 minutes data is present. b) Relative difference in the number of years of available data compared to 
-pre-processing values. A small relative difference indicates a small loss of data.'''
+DATA_VALIDATION_DIRECTORY     = '/home/data/Validation/AmeriFlux'
+MIN_NBR_YRS_DIRECTORY         = '/num-years'
+NUMPY_DIRECTORY               = '/npy'
+SAMPLING_PERCENTAGE_DIRECTORY = '/sampling-percentage'
+SNOW_COMEAU_DIRECTORY         = '/snow/comeau/FLUXNET_America/AMF_1990-2017'
+SNOW_DILUCA_DIRECTORY         = '/snow/diluca/FLUXNET_America/1990-2017/v1/TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE'
 
 
+# Step 0.2 : Define filenames
 
-# Step 1 : Obtain number of years ploting function parameters
+DATES_0_5_HR_FILENAME_1 = 'AMF_dates_'
+DATES_0_5_HR_FILENAME_2 = '_1990-2017_'
+DATES_0_5_HR_FILENAME_3 = '_0-5.npy'
+STATION_NAMES_FILENAME  = 'AMF_station-filelist_1990-2017_TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE.txt'
+SUMMARY_FILENAME        = 'AMF_summary_1990-2021_TA-SW_IN-SW_OUT-LW_IN-LW_OUT-H-LE.txt'
 
-for sampling_percentage in SAMPLING_PERCENTAGES :
 
-    for min_nbr_yrs in MIN_NBR_YRS :
+# Step 0.3 : Define delimiters
 
-        # Step 1.1 : Obtain station numbers and ids
+DATES_AND_DATA_0_5_HR_DELIMITER = ','
+STATION_NAMES_DELIMITER         = '_'
+VAR_NAMES_DELIMITER             = '-'
 
-        station_names_pathname = FILTERED_DATA_COMMON_DIRECTORY + '/' + FILTERED_DATA_SAMPLING_PERCENTAGES_DIRECTORY + sampling_percentage + '/' + FILTERED_DATA_MIN_NBR_YRS_DIRECTORY + min_nbr_yrs + '/' + STATION_NAMES_FILENAME
-        file_station_names     = open(station_names_pathname, 'r')
-        #pdb.set_trace()
 
-        station_nbrs_list = []
-        station_ids_list  = []
+# Step 0.4 : Define prefixes and suffixes
 
-        for line_station_names in file_station_names :
+AMF_PREFIX = 'AMF_'
+CSV_SUFFIX = '.csv'
+TXT_SUFFIX = '.txt'
 
-            amf_filename = os.path.basename(line_station_names)
-            station_nbr  = int(amf_filename.split(DELIMITER_STATION_NAMES)[2])
-            station_id   = amf_filename.split(DELIMITER_STATION_NAMES)[3]
 
-            station_nbrs_list.append(station_nbr)
-            station_ids_list.append(station_id)
+# Step 0.5 : Define special characters
+
+NEW_LINE_CHAR = '\n'
+NULL_CHAR     = ''
+READING_CHAR  = 'r'
+SLASH_BAR     = '/'
+STAR          = '*'
+
+
+# Step 0.6 : Define indexes
+
+DATES_0_5_HR_INDEX                   = 0
+DATES_3_HR_INDEX                     = 0
+DATES_AND_DATA_0_5_HR_PATHNAME_INDEX = 0
+DATES_AND_DATA_3_HR_PATHNAME_INDEX   = 0
+STATION_ID_INDEX                     = 3
+STATION_NBR_INDEX                    = 2
+VAR_NAMES_INDEX                      = 3
+
+
+# Step 0.7 : Define values
+
+DATA_ID                               = 'data'
+DATES_0_5_HR_FORMAT                   = '%Y%m%d%H%M'
+DATES_0_5_HR_HALF_TIME                = 15                  # half of half an hour is 15 minutes
+DATES_AND_DATA_0_5_NBR_OF_HEADER_ROWS = 3
+DATES_ID                              = 'dates'
+MIN_NBR_YRS                           = ['1', '2', '5']     # minimum number of years of available data
+MISSING_VALUE                         = '-9999'
+SAMPLING_PERCENTAGES                  = ['25', '50', '75']  # minimum sampling percentages
+T_FREQ                                = 0.5                 # in hours
+
+
+
+# Step 1 : Obtain variables of interest
+
+var_names      = STATION_NAMES_FILENAME.split(STATION_NAMES_DELIMITER, VAR_NAMES_INDEX)[VAR_NAMES_INDEX].replace(TXT_SUFFIX, NULL_CHAR)
+var_names_list = var_names.split(VAR_NAMES_DELIMITER)
+#pdb.set_trace()
+
+
+
+# Step 2 : Obtain station numbers and ids
+
+station_names_file_pathname = SNOW_COMEAU_DIRECTORY + SLASH_BAR + STATION_NAMES_FILENAME
+station_names_file          = open(station_names_file_pathname, READING_CHAR)
+
+station_nbrs_list = []
+station_ids_list  = []
+
+for station_names_line in station_names_file :
+
+    amf_filename = os.path.basename(station_names_line)
+    station_nbr  = amf_filename.split(STATION_NAMES_DELIMITER)[STATION_NBR_INDEX]
+    station_id   = amf_filename.split(STATION_NAMES_DELIMITER)[STATION_ID_INDEX]
+
+    station_nbrs_list.append(station_nbr)
+    station_ids_list.append(station_id)
+
+station_names_file.close()
+#pdb.set_trace()
+
+
+
+# Step 3 : Process dates and data for 0.5 hour averages
+
+total_dates_0_5_hr_list    = []
+nbr_of_entries_0_5_hr_list = []
+
+for station_nbr, station_id in zip(station_nbrs_list, station_ids_list) :
+
+
+    # Step 3.1 : Obtain file for the 0.5 hour average dates and data
+
+    dates_and_data_0_5_hr_pathname_pattern = DATA_VALIDATION_DIRECTORY + SLASH_BAR + AMF_PREFIX + station_id + STAR + CSV_SUFFIX
+    dates_and_data_0_5_hr_pathname         = glob.glob(dates_and_data_0_5_hr_pathname_pattern)[DATES_AND_DATA_0_5_HR_PATHNAME_INDEX]
+    dates_and_data_0_5_hr_file             = open(dates_and_data_0_5_hr_pathname, READING_CHAR)
+    #pdb.set_trace()
+
+
+    # Step 3.2 : Obtain variables for 0.5 hour average dates and data files
+
+    for line_nbr in range(0, DATES_AND_DATA_0_5_NBR_OF_HEADER_ROWS - 1) :
+        dates_and_data_0_5_hr_file.readline()
+
+    data_0_5_hr_var_names                                   = dates_and_data_0_5_hr_file.readline().split(DATES_AND_DATA_0_5_HR_DELIMITER)
+    data_0_5_hr_var_names[ len(data_0_5_hr_var_names) - 1 ] = data_0_5_hr_var_names[ len(data_0_5_hr_var_names) - 1].replace(NEW_LINE_CHAR, NULL_CHAR)
+    #pdb.set_trace()
+
+
+    # Step 3.3 : Extract relevant dates and data for 0.5 hour averages
+
+    data_0_5_hr_list  = []
+    dates_0_5_hr_list = []
+
+    for dates_and_data_0_5_hr_line in dates_and_data_0_5_hr_file :
+
+        data_0_5_hr_line_list = []
+
+        for var_name in var_names_list :
+
+            var_index = data_0_5_hr_var_names.index(var_name)
+            var_value = dates_and_data_0_5_hr_line.split(DATES_AND_DATA_0_5_HR_DELIMITER)[var_index].replace(NEW_LINE_CHAR, NULL_CHAR)
+
+            if ( var_value == MISSING_VALUE ) :  # if at least one value is missing ...
+                break                            # ... then the entire line is not worth keeping
+
+            else :
+                data_0_5_hr_line_list.append(float(var_value))
+
+        if ( len(data_0_5_hr_line_list) == len(var_names_list) ) :  # so as to not append an incomplete list
+
+            date_0_5_hr_string           = dates_and_data_0_5_hr_line.split(DATES_AND_DATA_0_5_HR_DELIMITER)[DATES_0_5_HR_INDEX]
+            date_0_5_hr_datetime         = datetime.datetime.strptime(date_0_5_hr_string, DATES_0_5_HR_FORMAT)
+            date_0_5_hr_central_datetime = date_0_5_hr_datetime + datetime.timedelta(minutes=DATES_0_5_HR_HALF_TIME)
+            date_0_5_hr_elapsed_time     = (date_0_5_hr_central_datetime - fc.constants.reference_date).total_seconds()
+
+            dates_0_5_hr_list.append(date_0_5_hr_elapsed_time)
+            data_0_5_hr_list.append(data_0_5_hr_line_list)
             #pdb.set_trace()
 
-        file_station_names.close()
-        #pdb.set_trace()
+    total_dates_0_5_hr_list.append(len(dates_0_5_hr_list))
+    nbr_of_entries_0_5_hr_list.append(len(dates_0_5_hr_list))
+    #pdb.set_trace()
 
 
-        # Step 1.2 : Obtain variable names
+    # Step 3.4 : Save relevant dates and data for 0.5 hour average
 
-        unfiltered_data_summary_pathname = UNFILTERED_DATA_DIRECTORY_W + '/' + FILTERED_DATA_FILENAME
+    dates_0_5_hr_array    = np.asarray(dates_0_5_hr_list)
+    dates_0_5_hr_filename = DATES_0_5_HR_FILENAME_1 + station_nbr + STATION_NAMES_DELIMITER + station_id + DATES_0_5_HR_FILENAME_2 + var_names + DATES_0_5_HR_FILENAME_3
+    dates_0_5_hr_pathname = SNOW_COMEAU_DIRECTORY + NUMPY_DIRECTORY + SLASH_BAR + dates_0_5_hr_filename
 
-        var_names      = FILTERED_DATA_FILENAME.split('_', 3)[3].replace('.txt', NULL_CHAR)
-        var_names_list = var_names.split('-')
-        #pdb.set_trace()
+    np.save(dates_0_5_hr_pathname, dates_0_5_hr_array)
 
+    data_0_5_hr_array    = np.asarray(data_0_5_hr_list)
+    data_0_5_hr_pathname = dates_0_5_hr_pathname.replace(DATES_ID, DATA_ID)
 
+    np.save(data_0_5_hr_pathname, data_0_5_hr_array)
+    #pdb.set_trace()
 
-        # Step 1.3 : Obtain number of years available for filtered data
 
-        filtered_data_pathname = FILTERED_DATA_COMMON_DIRECTORY + '/' + FILTERED_DATA_SAMPLING_PERCENTAGES_DIRECTORY + sampling_percentage + '/' + FILTERED_DATA_MIN_NBR_YRS_DIRECTORY + min_nbr_yrs + '/' + FILTERED_DATA_FILENAME
-        file_filtered_data     = open(filtered_data_pathname, 'r')
-        #pdb.set_trace()
+# Step 3.5 : Save number of years of data for 0.5 hour average
 
-        for line_nbr in range(0, NBR_OF_HEADER_ROWS_FILTERED_DATA) :
-            file_filtered_data.readline()
+dates_0_5_hr_t_freq = [ T_FREQ ] * len(total_dates_0_5_hr_list)
 
-        nbr_of_yrs_filtered_data_list = []  # represents data_2 entry
+total_dates_filename = SUMMARY_FILENAME
+total_dates_filepath = SNOW_COMEAU_DIRECTORY + SLASH_BAR + SUMMARY_FILENAME
 
-        for line_filtered_data in file_filtered_data :
-
-            nbr_of_yrs = float(line_filtered_data.split(DELIMITER_FILTERED_DATA)[4].replace(NEW_LINE_CHAR, NULL_CHAR))
-
-            nbr_of_yrs_filtered_data_list.append(nbr_of_yrs)
-            #pdb.set_trace()
-
-        file_filtered_data.close()
-        nbr_of_yrs_filtered_data_list.pop( len(nbr_of_yrs_filtered_data_list) - 1 )
-        #pdb.set_trace()
-
-
-
-        # Step 1.4 : Obtain number of years available for unfiltered data
-
-        # Step 1.4.1 : Obtain summary function parameters
-
-        total_unfiltered_data_list = []
-
-        for station_id in station_ids_list :
-
-            unfiltered_data_pathname_pattern = UNFILTERED_DATA_DIRECTORY_R + '/' + 'AMF_' + station_id + '*' + '.csv'
-            unfiltered_data_pathname         = glob.glob(unfiltered_data_pathname_pattern)[0]
-            file_unfiltered_data             = open(unfiltered_data_pathname, 'r')
-
-            for line_nbr in range(0, NBR_OF_HEADER_ROWS_UNFILTERED_DATA - 1) :
-                file_unfiltered_data.readline()
-
-            var_names_unfiltered_data                                       = file_unfiltered_data.readline().split(',')
-            var_names_unfiltered_data[ len(var_names_unfiltered_data) - 1 ] = var_names_unfiltered_data[ len(var_names_unfiltered_data) - 1].replace(NEW_LINE_CHAR, NULL_CHAR)
-            #pdb.set_trace()
-
-            unfiltered_data_list  = []
-            unfiltered_dates_list = []
-
-            for line_unfiltered_data in file_unfiltered_data :
-
-                unfiltered_data_line_list = []
-
-                for var_name in var_names_list :
-
-       	       	    var_index = var_names_unfiltered_data.index(var_name)
-                    var_value = line_unfiltered_data.split(',')[var_index].replace(NEW_LINE_CHAR, NULL_CHAR)
-
-                    if ( var_value == '-9999' ) :  # if at least one value is missing ...
-                        break                      # ... then the entire line is not worth keeping 
-
-                    else :
-                        unfiltered_data_line_list.append(float(var_value))
-
-                if ( len(unfiltered_data_line_list) == len(var_names_list) ) :  # so as to not append an incomplete list
-                    
-                    unfiltered_data_list.append(unfiltered_data_line_list)
-
-                    unfiltered_date_string               = line_unfiltered_data.split(',')[0]
-                    unfiltered_date_datetime             = datetime.datetime.strptime(unfiltered_date_string, '%Y%m%d%H%M')
-                    unfiltered_central_date_datetime     = unfiltered_date_datetime + datetime.timedelta(minutes=15)
-                    unfiltered_central_date_elapsed_time = (unfiltered_central_date_datetime - fc.constants.reference_date).total_seconds() 
-
-                    unfiltered_dates_list.append(unfiltered_central_date_elapsed_time)
-                    #pdb.set_trace()
-
-            total_unfiltered_data_list.append(len(unfiltered_data_list))
-
-
-            # Step 1.4.2 : Save unfiltered data and dates
-
-            unfiltered_data_array  = np.asarray(unfiltered_data_list)
-
-            unfiltered_data_filename = UNFILTERED_DATA_FILENAME_1 + str(station_nbr) + '_' + station_id + UNFILTERED_DATA_FILENAME_2 + var_names + UNFILTERED_DATA_FILENAME_3
-            unfiltered_data_pathname = UNFILTERED_DATA_DIRECTORY_W + '/npy/' + unfiltered_data_filename
-
-            np.save(unfiltered_data_pathname, unfiltered_data_array)
-
-            unfiltered_dates_array = np.asarray(unfiltered_dates_list)
-
-            unfiltered_dates_pathname = unfiltered_data_pathname.replace('data', 'dates')
-
-            np.save(unfiltered_dates_pathname, unfiltered_dates_array)            
-
-            pdb.set_trace()
-    
-        t_freq = [0.5] * len(total_unfiltered_data_list)
-
-
-        # Step 1.4.3 : Create summary file for unfiltered data
-
-        fc.print_summary(total_unfiltered_data_list, unfiltered_data_summary_pathname, t_freq, var_names) 
-        #pdb.set_trace()
-
-
-        # Step 1.4.4 : Read summary file for unfiltered data
-
-        unfiltered_data_pathname = unfiltered_data_summary_pathname
-        file_unfiltered_data     = open(unfiltered_data_pathname, 'r')
-
-        for line_nbr in range(0, NBR_OF_HEADER_ROWS_FILTERED_DATA) :
-            file_unfiltered_data.readline()
-
-        nbr_of_yrs_unfiltered_data_list = []  # represents data_2 entry
-
-        for line_unfiltered_data in file_unfiltered_data :
-
-            nbr_of_yrs = float(line_unfiltered_data.split(DELIMITER_FILTERED_DATA)[4].replace(NEW_LINE_CHAR, NULL_CHAR))
-
-            nbr_of_yrs_unfiltered_data_list.append(nbr_of_yrs)
-            #pdb.set_trace()
-
-        file_unfiltered_data.close()
-        nbr_of_yrs_unfiltered_data_list.pop( len(nbr_of_yrs_unfiltered_data_list) - 1 )
-        #pdb.set_trace()
-
-
-
-        # Step 2 : Create plots
-
-        label_post_processing = LABEL_POST_PROCESSING_1 + min_nbr_yrs + LABEL_POST_PROCESSING_2 + sampling_percentage + LABEL_POST_PROCESSING_3
-        station_numbers       = np.array(station_nbrs_list)
-        station_names         = np.array(station_ids_list)
-        data_1                = np.array(nbr_of_yrs_unfiltered_data_list)
-        data_2                = np.array(nbr_of_yrs_filtered_data_list)
-
-        plot_filename = 'nbr-of-years-of-data_' + FILTERED_DATA_SAMPLING_PERCENTAGES_DIRECTORY + sampling_percentage + '_' + FILTERED_DATA_MIN_NBR_YRS_DIRECTORY + min_nbr_yrs + '.png'
-        plot_pathname = UNFILTERED_DATA_DIRECTORY_W + '/png/' + plot_filename
-
-        mcp.Make_comparison_plot(station_numbers, station_names, data_1, data_2, Y_AXIS_LABEL, LABEL_PRE_PROCESSING, label_post_processing, CAPTION, plot_pathname)
-        #pdb.set_trace()
-
-
-        """
-        # Step 3 : Obtain average values ploting function parameters
-
-        var_names_list = var_names.split('-')
-
-        time_length_list = [ 365 * 24 ] * len(total_unfiltered_data_list)
-
-        #pdb.set_trace()
-        """
+fc.print_summary(total_dates_0_5_hr_list, total_dates_filepath, dates_0_5_hr_t_freq, var_names)
