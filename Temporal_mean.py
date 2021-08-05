b5362bfecd3327ef41b05475d9eab67c28ab0428
Elise Comeau <comeau.elise@courrier.uqam.ca>
2021-08-05 11:30:30 -0400

    Added a new parameter that lets the user choose the starting point of the temporal averaging instead of starting from the first date of the input data.

--- Temporal_mean.py
+++ Temporal_mean.py
@@ -1,8 +1,12 @@
 import numpy as np
 import sys
+import datetime
+from datetime import timedelta
+import fluxnet_classes as fc
+import pdb
 
 
-def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_long, min_data_percentage) :
+def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_long, central_time, min_data_percentage) :
 
 
     """
@@ -19,6 +23,10 @@ def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_lo
         
         t_freq_long (int)           : Temporal frequency associated with data_long_period, expressed in seconds.
         
+        central_time (float)        : Time about which the temporal mean will be calculated, expressed in hours ranging from 0 to 24 (0 inclusive, 24 exclusive). For example, if 
+                                      t_freq_long = 10800 (ie, 3 hours) and we would like to divide a day starting at midnight (00:00 to 03:00, 03:00 to 06:00, etc.), we could
+                                      enter 1.5 (the center between 00:00 and 03:00) or 4.5, among other values. 
+
         min_data_percentage (float) : Minimum amount of data that must be present over the shorter time period to be included in the mean of the longer time period. Expressed
                                       as a percentage from 0 to 100. 
     
@@ -34,7 +42,7 @@ def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_lo
 
     Created       : June 3rd, 2021
 
-    Last modified : June 15th, 2021
+    Last modified : June 17th, 2021
 
 
     """
@@ -57,6 +65,11 @@ def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_lo
         print('Temporal frequencies appear to have been switched. Temporal mean will not be calculated.\n')
         sys.exit(0) 
 
+    elif ( ( central_time < 0 ) or ( central_time >= 24 ) ) :
+
+        print('The central time is out of bounds. Temporal mean will not be calculated.\n')
+        sys.exit(0)
+
     elif ( ( min_data_percentage < 0 ) or ( min_data_percentage > 100 ) ) :
 
         print('The minimum data percentage is out of bounds. Temporal mean will not be calculated.\n')
@@ -67,12 +80,52 @@ def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_lo
         print('Data, dates and minimum data percentage are in order. Temporal mean will be calculated.\n')
 
 
-        # Step 2 : Calculate the mean over the longer time period.
+        # Step 2 : Find the time at which the calculations will begin
+	
+        # converting the central time to the same format as dates in dates_short_period
+
+        first_time     = dates_short_period[0]
+        first_datetime = fc.constants.reference_date + timedelta(seconds=first_time) # the dates represent time deltas, in seconds, since reference_date (ie., January 1st 1971)
+
+        if ( ( central_time - 0.5 * ( t_freq_long / 3600 ) ) >= 0 ) :
+            cutoff = central_time - 0.5 * ( t_freq_long / 3600 )      # cutoff represents the transition between two consecutive periods; eg. : the cutoff between 00:00 - 03:00 and 03:00 - 06:00 is 03:00
+
+        else :
+            cutoff = central_time + 0.5 * ( t_freq_long / 3600 )
+
+        cutoff_hour   = int(cutoff)
+        cutoff_minute = int((cutoff - cutoff_hour) * 60)
+
+#        pdb.set_trace()
+
+        initial_datetime = datetime.datetime(first_datetime.year, first_datetime.month, first_datetime.day, cutoff_hour, cutoff_minute)
+        initial_time     = (initial_datetime - fc.constants.reference_date).total_seconds()                                                 # time at which the calculations might begin
+
+        elapsed_time = first_time - initial_time
+#        pdb.set_trace()
+
+        # finding the time at which calculations should begin
+
+        while ( elapsed_time < 0 ) :                # initial_time is 'too late'; it must be moved backwards in time
+  
+            initial_time = initial_time - t_freq_long
+            elapsed_time = first_time - initial_time            
+#            pdb.set_trace()
+
+
+        while ( elapsed_time >= t_freq_long ) :   # initial_time is 'too early'; it must be moved forwards in time
+             
+            initial_time = initial_time + t_freq_long
+            elapsed_time = first_time - initial_time
+
+#        pdb.set_trace() 
+  
+
+        # Step 3 : Calculate the mean over the longer time period.
 
         contiguous_data    = []
         contiguous_dates   = []
         total_rows         = data_short_period.shape[0]
-        initial_time       = dates_short_period[0]       # time at which the averaging period begins; we begin counting at the first row
         initial_row        = 0                           # row at whihch temporal averaging begins        
         data_per_period    = t_freq_long / t_freq_short  # quantity of data per period, provided all the data is present
         offset             = 0                           # necessary to include the correct amount of rows in the mean calculation 
@@ -104,11 +157,10 @@ def Temporal_mean(data_short_period, dates_short_period, t_freq_short, t_freq_lo
             if ( data_percentage >= min_data_percentage ) :
 
                 contiguous_data.append(data_short_period[row,:])
-                contiguous_weights.append(1)
                 contiguous_dates.append(initial_time + t_freq_long / 2)
 
 
-        # Step 3 : Return the results from the temporal averaging.
+        # Step 4 : Return the results from the temporal averaging.
 
         data_long_period  = np.array(contiguous_data)
         dates_long_period = np.array(contiguous_dates)
